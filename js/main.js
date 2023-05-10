640c2e8f5ffa250723eb8d0eee7b4c48771aa0d2
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-10 11:56:42 +0200

    affossato

--- js/main.js
+++ js/main.js
@@ -21,7 +21,25 @@ play.addEventListener("click", function () {
         //aggiungiamo la classe .square ai quadrati
         newSquare.classList.add("square");
     }
+
+    //selezioniamo i quadrati
+    const squares = document.getElementsByClassName("square");
+    console.log(squares)
+
+    //creiamo un ciclo per tutti i quadrati
+    for (let i = 0; i < squares.length; i++) {
+
+        //aggiungiamo funzioni al click dei quadrati
+        squares[i].addEventListener("click", function () {
+
+            //aggiungiamo la classe .blue al quadrato cliccato
+            const blueSquare = colorSquare ("blue");
+            console.log(blueSquare);
+        });
+    }
 });
+
+
 
 
 
@@ -33,4 +51,10 @@ function createSquare() {
     const newElement = document.createElement("div");
     newElement.classList.add("square");
     return newElement;
+}
+
+//aggiunta classe per colore
+function colorSquare(color) {
+    const elementColor = document.getElementsByClassName("square");
+    elementColor.classList.add(color);  
 }
